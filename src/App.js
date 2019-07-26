327fbabb6b2053a8e70ba560bbf95912969169cc
Renato Lima <limar@sureskills.com>
2019-07-26 20:41:34 +0100

    updating project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './config/ReactotronConfig';
+
 import { Provider } from 'react-redux';
 
 import store from './store';
 
-import TodoList from './TodoList';
+
+console.tron.log('Testando');
 
 const App = () => (
     <Provider store={store}>
-      <TodoList />
+      <h1>example 1</h1>
     </Provider>
 );
 
